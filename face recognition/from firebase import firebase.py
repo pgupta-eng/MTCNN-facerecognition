9bf305e2c999d62568bfa7f1c84bb54370be3c74
pgupta-eng <56155282+pgupta-eng@users.noreply.github.com>
2020-04-02 22:40:01 +0530

    Update from firebase import firebase.py

--- face recognition/from firebase import firebase.py
+++ face recognition/from firebase import firebase.py
@@ -1,13 +1,21 @@
-from firebase import firebase
+from fire import firebase
 
+firebase = firebase.FirebaseApplication('https://collapp-2605.firebaseio.com', None)
 
-firebase = firebase.FirebaseApplication("https://pythondbtest-f7d3d.firebaseio.com/",None)
-data = {
-    'Name': 'Prerna Gupta',
-    'Email' : 'prernaajigupta@gmail.com',
-    'contact number': 6353420534,
+while True:
 
-}
+    #rollno =(input('Enter no here...'))
+    teacher=input('Enter teacher here...')
+    #ACLASS = input('Enter attended classes here...')
+    #date = input('Enter date here...')
+    LAST_ATT = firebase.get('/Attendance' + teacher, None)
+    data = {
+        'rollno': rollno
+    }
 
-result = firebase.post('pythondbtest-f7d3d/student',data)
-print(result)
+    #result = firebase.post('Attendance',{'gjh':data})
+
+    resultput = firebase.put('Attendance'+teacher,rollno,{"teacher":teacher,'total class':'26','attended classes':ACLASS,'last class':date})
+
+    print(LAST_ATT)
+    print(resultput)
